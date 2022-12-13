fa537fdb555fcb30ba4e261c0b8b83fb426c1264
CapSap <cshotam@gmail.com>
2022-12-13 17:32:14 +1100

    rename home and reciepe to xxxxScreen

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 
-export const Recipe = () => {
+export const HomeScreen = () => {
   return (
     <View>
-      <Text>{'RecipeScreen'}</Text>
+      <Text>{'HomeScreen'}</Text>
     </View>
   );
 };

72a63a82ad7ee3297890c73abd56d0958fe034d6
CapSap <cshotam@gmail.com>
2022-12-20 21:41:24 +1100

    add home and search componenets

--- src/components/BottomTabs.navigator.tsx
+++ src/components/BottomTabs.navigator.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {HomeScreen} from './HomeScreen';
 import {RecipeScreen} from './RecipeScreen';
+import {SearchScreen} from './SearchScreen';
+import {ProfileScreen} from './ProfileScreen';
 
 const BottomTabs = createBottomTabNavigator();
 
@@ -9,7 +11,9 @@ export const BottomTabsNavigator: React.FC = () => {
   return (
     <BottomTabs.Navigator>
       <BottomTabs.Screen name="Home" component={HomeScreen} />
+      <BottomTabs.Screen name="Search" component={SearchScreen} />
       <BottomTabs.Screen name="View recipe" component={RecipeScreen} />
+      <BottomTabs.Screen name="Profile" component={ProfileScreen} />
     </BottomTabs.Navigator>
   );
 };

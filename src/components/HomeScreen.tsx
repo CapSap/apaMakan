d2726b44c6703c12d46d66374a3eccdfaec09986
CapSap <cshotam@gmail.com>
2022-12-23 22:22:59 +1100

    nested navigation attempt

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -2,23 +2,37 @@ import React from 'react';
 import {View, FlatList, StyleSheet} from 'react-native';
 import {Recipe} from '../types';
 import {RecipeCard} from './ReceipeCard';
-
 import {useAppContext} from '../App.provider';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {RecipeDetail} from './RecipeDetail';
 
 export const HomeScreen = () => {
   //get dummyData from useContext()
   const recipeData: Recipe[] = useAppContext().appState;
 
+  const Tab = createNativeStackNavigator();
+
+  const List = () => {
+    return (
+      <View style={styles.container}>
+        <FlatList
+          contentContainerStyle={styles.list}
+          data={recipeData}
+          renderItem={RecipeCard}
+          keyExtractor={(item, index) => item.recipeName + item.id + index}
+          numColumns={2}
+        />
+      </View>
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <FlatList
-        contentContainerStyle={styles.list}
-        data={recipeData}
-        renderItem={RecipeCard}
-        keyExtractor={(item, index) => item.recipeName + item.id + index}
-        numColumns={2}
-      />
-    </View>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={List} />
+      <Tab.Screen name="Detail" component={RecipeDetail} />
+    </Tab.Navigator>
   );
 };
 

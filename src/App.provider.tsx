62b484985b762749ee52ab861ca70db3e26cb2ff
forginater <alex.w.forge@gmail.com>
2022-12-23 16:23:34 +1100

    put dummyData into a useState hook in App.provider.tsx & passed it to the ContextProvider

--- src/App.provider.tsx
+++ src/App.provider.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import {dummyData} from './dummyData';
+import {Recipe} from './types';
 
 type AppContextType = {
-  msg: string;
+  data: Recipe[];
 };
 
-const defaultValue = {msg: ''};
+const initialRecipes = {data: dummyData};
 
-const AppContext = React.createContext<AppContextType>(defaultValue);
+const AppContext = React.createContext<AppContextType>(initialRecipes);
 
 //Create a context provider
 export const AppProvider: React.FC = ({children}) => {
+  //store dummyData in state
+  const [recipesState, setRecipesState] = React.useState<Recipe[]>(dummyData);
+
   return (
-    <AppContext.Provider value={{msg: 'newMsgValue'}}>
+    <AppContext.Provider value={{data: recipesState}}>
       {children}
     </AppContext.Provider>
   );

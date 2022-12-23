62a43059c112f84a502a7b5b782ef4c7212bed0b
CapSap <cshotam@gmail.com>
2022-12-23 17:52:38 +1100

    render a list of recipes

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -1,34 +1,31 @@
 import React, {useState} from 'react';
-import {Text, View, ScrollView, Button} from 'react-native';
+import {
+  Text,
+  View,
+  ScrollView,
+  Button,
+  FlatList,
+  StyleSheet,
+} from 'react-native';
 import {HomeProps} from './BottomTabs.navigator';
 import {Recipe} from '../types';
+import {RecipeCard} from './ReceipeCard';
 
 export const HomeScreen = ({route}: HomeProps) => {
   const recipeData: Recipe[] = route.params.data;
 
   return (
-    <View>
-      <Text>{'HomeScreen'}</Text>
-      <DisplayRecipeData recipes={recipeData} />
-    </View>
+    <FlatList
+      style={styles.container}
+      data={recipeData}
+      renderItem={RecipeCard}
+      keyExtractor={item => item.id}
+    />
   );
 };
 
-//Testing: DisplayRecipeData displays output of recipeData received through route.params to check contents
-const DisplayRecipeData = (props: {recipes: Recipe[]}) => {
-  const [showRecipes, setShowRecipes] = useState(false);
-  const [buttonTitle, setButtonTitle] = useState('Press to Display RecipeData');
-
-  return (
-    <ScrollView>
-      <Button
-        title={buttonTitle}
-        onPress={() => {
-          setShowRecipes(current => !current);
-          setButtonTitle('Press to Hide RecipeData');
-        }}
-      />
-      {showRecipes && <Text>{JSON.stringify(props.recipes, null, 4)}</Text>}
-    </ScrollView>
-  );
-};
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+  },
+});

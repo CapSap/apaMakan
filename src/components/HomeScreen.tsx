dca3ea9a5ab1c05b619fccff159ee0f9bd986541
CapSap <cshotam@gmail.com>
2022-12-26 11:42:48 +1100

    move receipeCard into homescreen

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import {View, FlatList, StyleSheet} from 'react-native';
+import {View, FlatList, StyleSheet, TouchableOpacity, Text} from 'react-native';
 import {Recipe} from '../types';
-import {RecipeCard} from './ReceipeCard';
+// import {RecipeCard} from './ReceipeCard';
 import {useAppContext} from '../App.provider';
 
-import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {RecipeDetail} from './RecipeDetail';
 
@@ -14,13 +13,25 @@ export const HomeScreen = () => {
 
   const Stack = createNativeStackNavigator();
 
-  const RecipeList = () => {
+  const RecipeList = ({navigation}) => {
     return (
       <View style={styles.container}>
         <FlatList
           contentContainerStyle={styles.list}
           data={recipeData}
-          renderItem={RecipeCard}
+          renderItem={({item}: {item: Recipe}) => {
+            const onPress = () => {
+              navigation.navigate('Detail', {item});
+            };
+
+            return (
+              <TouchableOpacity onPress={onPress} style={styles.card}>
+                <Text style={styles.time}>{item.recipeDuration}m</Text>
+                <Text style={styles.image}>{item.recipeImage}</Text>
+                <Text style={styles.title}>{item.recipeName}</Text>
+              </TouchableOpacity>
+            );
+          }}
           keyExtractor={(item, index) => item.recipeName + item.id + index}
           numColumns={2}
         />
@@ -43,5 +54,30 @@ const styles = StyleSheet.create({
 
   list: {
     alignContent: 'space-around',
+  },
+  card: {
+    backgroundColor: 'teal',
+    height: 150,
+    width: 150,
+    border: 1,
+    margin: 10,
+    borderRadius: 10,
+    fontSize: 20,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  time: {
+    alignSelf: 'flex-start',
+    margin: 5,
+    padding: 2,
+    width: 35,
+  },
+  image: {
+    fontSize: 30,
+  },
+  title: {
+    fontSize: 20,
+    padding: 2,
+    paddingBottom: 25,
   },
 });

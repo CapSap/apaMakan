d57aabfb8a657ea9622f31d184be285d56eecbd4
CapSap <cshotam@gmail.com>
2022-12-26 22:50:35 +1100

    basic detail screen with some text

--- src/components/RecipeDetail.tsx
+++ src/components/RecipeDetail.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {FlatList, ScrollView, StyleSheet, Text, View} from 'react-native';
 import {DetailProps} from '../types';
 
 export const RecipeDetail = ({route}: DetailProps) => {
-  const {item} = route.params;
-  console.log(item);
+  const {recipe} = route.params;
+  console.log(recipe);
   return (
     <View>
-      <Text>{item.recipeName}</Text>
+      <Text>{recipe.recipeName}</Text>
+      <Text>Cooking time: {recipe.recipeDuration} minutes</Text>
+      <Text>{recipe.recipeDescription}</Text>
+      <FlatList
+        ListHeaderComponent={<Text>Ingredients</Text>}
+        data={recipe.ingredients}
+        renderItem={({item}) => (
+          <Text>
+            Qty: {item.qty} - {item.ingredientName}
+          </Text>
+        )}
+      />
+      <FlatList
+        ListHeaderComponent={<Text>Steps</Text>}
+        data={recipe.steps}
+        renderItem={({item, index}) => (
+          <Text>
+            {index + 1}: {item}
+          </Text>
+        )}
+      />
     </View>
   );
 };
